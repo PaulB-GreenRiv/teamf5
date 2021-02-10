530bbc9ab95d6d8e8d757e93ba054021a7f8cdca
PaulB-GreenRiv <77860962+PaulB-GreenRiv@users.noreply.github.com>
2021-02-09 18:09:49 -0800

    Add files via upload

---
+++ GroupProjectTest/GroupProjectTest/category.php
@@ -0,0 +1,181 @@
+<?php
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    include('includes/head.html');
+?>
+<body>
+
+<!-- Header start (May want to make a header html file)-->
+    <?php
+        include('includes/header.html');
+    ?>
+<!-- Header end -->
+
+    <div class="container" id="main">
+        <!--Include headers for each category-->
+        <div id="catAgriculture">
+            <h1>Agriculture</h1>
+            <ul>
+                <li>
+                    <h4><a href="https://compostwerks.com/contact-us" target="_blank">CompostWorks LLC.</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="https://ecologyartisans.com" target="_blank">Ecology Artisans</a></h4>
+                    <p>Location: San Diego</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="https://farmbot.io" target="_blank">FarmBot</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="http://compostingtechnology.com/" target="_blank">Green Mountain Technologies</a></h4>
+                    <p>Location: Bainbridge Island, Washington</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="http://interraenergy.us" target="_blank">Interra Energy</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="https://www.kandarianorganicfarms.com" target="_blank">Kandarian Organic Farms</a></h4>
+                    <p>Location: Los Osos, CA</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Kitchen Table Consultants</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="http://www.lundberg.com" target="_blank">Lundberg Family Farms</a></h4>
+                    <p>Location: Richvale, CA</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Mastadon Family Farm</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>McManis Family Ranch</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="https://naturesfootprint.com" target="_blank">Nature's Footprint</a></h4>
+                    <p>Location: Bellingham, WA</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Organic Valley</a></h4>
+                    <p>Location: La Farge, WI</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Pharmersea</a></h4>
+                    <p>Location: Santa Barbara</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>San Diego Seed Company</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Spiked Soil</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Tablas Creek</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="https://unclejimswormfarm.com/" target="_blank">Uncle Jim's Worm Farm</a></h4>
+                    <p>Location:</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a>Upcyle and Company</a></h4>
+                    <p>Location: San Diego</p>
+                    <p>Tagline:</p>
+                </li>
+                <li>
+                    <h4><a href="https://www.vitalchoice.com/" target="_blank">Vital Choice</a></h4>
+                    <p>Location: Bellingham, WA</p>
+                    <p>Tagline:</p>
+                </li>
+            </ul>
+        </div>
+        <!--
+        <div id="catCircularEconomy">
+            <h1>Circular Economy</h1>
+            <ul></ul>
+        </div>
+        <div id="catClothing">
+            <h1>Clothing</h1>
+            <ul></ul>
+        </div>
+        <div id="catConsumerGoods">
+            <h1>Consumer Goods</h1>
+            <ul></ul>
+        </div>
+        <div id="catEcology">
+            <h1>Ecology</h1>
+            <ul></ul>
+        </div>
+        <div id="catEducation">
+            <h1>Education</h1>
+            <ul></ul>
+        </div>
+        <div id="catEnergy">
+            <h1>Energy</h1>
+            <ul></ul>
+        </div>
+        <div id="catHealthcare">
+            <h1>Healthcare</h1>
+            <ul></ul>
+        </div>
+        <div id="catHousing">
+            <h1>Housing</h1>
+            <ul></ul>
+        </div>
+        <div id="catLighting">
+            <h1>Lighting</h1>
+            <ul></ul>
+        </div>
+        <div id="catManufacturing">
+            <h1>Manufacturing</h1>
+            <ul></ul>
+        </div>
+        <div id="catTransportation">
+            <h1>Transportation</h1>
+            <ul></ul>
+        </div>
+        <div id="catWastewater">
+            <h1>Wastewater</h1>
+            <ul></ul>
+        </div>
+        <div id="catWater">
+            <h1>Water</h1>
+            <ul></ul>
+        </div>
+        -->
+
+        <!--List several companies in category-->
+        <!--Company names link to site-->
+    </div>
+
+    <?php
+        include('includes/footer.html');
+    ?>
+</body>
+</html>
